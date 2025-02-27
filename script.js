d6666785629b43517df4c85b9891cbbbddf70524
Pixelis0P <131791502+PixelMCN@users.noreply.github.com>
2025-02-27 14:01:44 +0000

    updated

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile navigation menu
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
